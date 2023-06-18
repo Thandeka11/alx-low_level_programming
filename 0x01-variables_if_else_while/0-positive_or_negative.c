56417d9d154b8625741860d89ec4b49cf066e797
Thandeka11 <thandekaa49@gmail.com>
2023-06-18 16:24:27 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+/**
+ * main - generate random numbers to detect -ve or +ve
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int n;
 
-int main() {
-    int n;
-    srand(time(0)); // Seed the random number generator with current time
-    
-    n = rand() % 201 - 100; // Generate a random number between -100 and 100
-    
-    printf("The number: %d\n", n);
-    
-    if (n > 0) {
-        printf("is positive\n");
-    } else if (n == 0) {
-        printf("is zero\n");
-    } else {
-        printf("is negative\n");
-    }
-    
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
