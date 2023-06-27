beb62fa423b3102c656300ecb647945dc559d15a
Thandeka11 <thandekaa49@gmail.com>
2023-06-27 04:49:59 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,8 @@
-void reset_to_98(int *n) {
-    *n = 98;
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98.
+ * @n: Pointer to an integer.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
