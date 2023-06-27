cc59ec0fdd2d87e6368bacc025c3b569e0dc2027
Thandeka11 <thandekaa49@gmail.com>
2023-06-27 06:13:07 -0700

    puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,26 @@
-#include <unistd.h>
-
 /**
- * puts_half - Prints the second half of a string followed by a new line.
+ * puts_half - Prints the second half of a string.
  * @str: Pointer to a string.
  */
 void puts_half(char *str)
 {
 	int length = 0;
-	int start;
+	int half_length;
+	int i;
 
-	/* Calculate the length of the string */
+	if (str == NULL)
+		return;
+
 	while (str[length] != '\0')
 		length++;
 
-	start = length / 2;
+	if (length % 2 == 0)
+		half_length = length / 2;
+	else
+		half_length = (length - 1) / 2;
 
-	/* Print the second half of the string */
-	while (str[start] != '\0')
-	{
-		write(1, &str[start], 1);
-		start++;
-	}
+	for (i = half_length; i < length; i++)
+		_putchar(str[i]);
 
-	write(1, "\n", 1);
+	_putchar('\n');
 }
