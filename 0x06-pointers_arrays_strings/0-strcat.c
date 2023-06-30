aac24caf318642c4e07998c89b91d20b9ff6e1b7
Thandeka11 <thandekaa49@gmail.com>
2023-06-30 13:06:30 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,20 @@
-#include <stddef.h>
-
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-    char *dest_ptr = dest;
-    
-    // Find the end of the destination string
-    while (*dest_ptr != '\0')
-        dest_ptr++;
-    
-    // Concatenate the source string to the destination string
-    while (*src != '\0') {
-        *dest_ptr = *src;
-        dest_ptr++;
-        src++;
-    }
-    
-    // Add the terminating null byte
-    *dest_ptr = '\0';
-    
-    return dest;
+	int dest_len = 0;
+	int i = 0;
+
+	// Find the length of the destination string
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	// Append the source string to the destination string
+	while (src[i] != '\0') {
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	// Add the terminating null byte
+	dest[dest_len + i] = '\0';
+
+	return dest;
 }
