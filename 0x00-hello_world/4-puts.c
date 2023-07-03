0d7f947fe3d54ce707415553e10309f5267fc49b
Thandeka11 <thandekaa49@gmail.com>
2023-07-03 02:39:03 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
+/**
+ * main -it prints out output using put 
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}		
