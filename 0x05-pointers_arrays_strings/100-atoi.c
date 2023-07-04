984acea9cf334923bac7c0ef8710e1f4cb44386f
Thandeka11 <thandekaa49@gmail.com>
2023-07-04 01:51:48 -0700

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	/* Handle leading whitespace */
+	while (s[i] == ' ')
+		i++;
+
+	/* Handle optional sign */
+	if (s[i] == '-' || s[i] == '+') {
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+
+	/* Convert the string to an integer */
+	while (s[i] >= '0' && s[i] <= '9') {
+		/* Check for integer overflow */
+		if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && s[i] - '0' > (INT_MAX % 10))) {
+			/* Handle overflow case */
+			if (sign == 1)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
+
+	return sign * result;
+}
